6a02d177ac3c1c405b2baf33a3c2a97c4c3ca604
Dominik AD <dominik716@wp.pl>
2021-04-21 19:42:58 +0200

    First working version

--- Salta/MainWindow.xaml.cs
+++ Salta/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +15,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace Salta
 {
     /// <summary>
@@ -20,9 +23,77 @@ namespace Salta
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+		private ObservableCollection<SaltaPiece> Pieces;
+		private Point LastSelectedPosition = new Point(-1,-1);
+
+		public MainWindow()
         {
-            InitializeComponent();
-        }
+			InitializeComponent();
+
+			this.Pieces = new ObservableCollection<SaltaPiece>() {
+				new SaltaPiece{Pos=new Point(0, 9), Type=PieceType.Star_1, Player=Player.Green},
+                new SaltaPiece{Pos=new Point(2, 9), Type=PieceType.Star_2, Player=Player.Green},
+                new SaltaPiece{Pos=new Point(4, 9), Type=PieceType.Star_3, Player=Player.Green},
+                new SaltaPiece{Pos=new Point(6, 9), Type=PieceType.Star_4, Player=Player.Green},
+                new SaltaPiece{Pos=new Point(8, 9), Type=PieceType.Star_5, Player=Player.Green},
+				new SaltaPiece{Pos=new Point(1, 8), Type=PieceType.Moon_1, Player=Player.Green},
+				new SaltaPiece{Pos=new Point(3, 8), Type=PieceType.Moon_2, Player=Player.Green},
+				new SaltaPiece{Pos=new Point(5, 8), Type=PieceType.Moon_3, Player=Player.Green},
+				new SaltaPiece{Pos=new Point(7, 8), Type=PieceType.Moon_4, Player=Player.Green},
+				new SaltaPiece{Pos=new Point(9, 8), Type=PieceType.Moon_5, Player=Player.Green},
+				new SaltaPiece{Pos=new Point(0, 7), Type=PieceType.Point_1, Player=Player.Green},
+				new SaltaPiece{Pos=new Point(2, 7), Type=PieceType.Point_2, Player=Player.Green},
+				new SaltaPiece{Pos=new Point(4, 7), Type=PieceType.Point_3, Player=Player.Green},
+				new SaltaPiece{Pos=new Point(6, 7), Type=PieceType.Point_4, Player=Player.Green},
+				new SaltaPiece{Pos=new Point(8, 7), Type=PieceType.Point_5, Player=Player.Green},
+				
+				new SaltaPiece{Pos=new Point(9, 0), Type=PieceType.Star_1, Player=Player.Red},
+				new SaltaPiece{Pos=new Point(7, 0), Type=PieceType.Star_2, Player=Player.Red},
+				new SaltaPiece{Pos=new Point(5, 0), Type=PieceType.Star_3, Player=Player.Red},
+				new SaltaPiece{Pos=new Point(3, 0), Type=PieceType.Star_4, Player=Player.Red},
+				new SaltaPiece{Pos=new Point(1, 0), Type=PieceType.Star_5, Player=Player.Red},
+				new SaltaPiece{Pos=new Point(8, 1), Type=PieceType.Moon_1, Player=Player.Red},
+				new SaltaPiece{Pos=new Point(6, 1), Type=PieceType.Moon_2, Player=Player.Red},
+				new SaltaPiece{Pos=new Point(4, 1), Type=PieceType.Moon_3, Player=Player.Red},
+				new SaltaPiece{Pos=new Point(2, 1), Type=PieceType.Moon_4, Player=Player.Red},
+				new SaltaPiece{Pos=new Point(0, 1), Type=PieceType.Moon_5, Player=Player.Red},
+				new SaltaPiece{Pos=new Point(9, 2), Type=PieceType.Point_1, Player=Player.Red},
+				new SaltaPiece{Pos=new Point(7, 2), Type=PieceType.Point_2, Player=Player.Red},
+				new SaltaPiece{Pos=new Point(5, 2), Type=PieceType.Point_3, Player=Player.Red},
+				new SaltaPiece{Pos=new Point(3, 2), Type=PieceType.Point_4, Player=Player.Red},
+				new SaltaPiece{Pos=new Point(1, 2), Type=PieceType.Point_5, Player=Player.Red},
+
+				new SaltaPiece{Pos=new Point(-1, -1), Type=PieceType.Selection, Player=Player.Green},
+			};
+
+			this.ChessBoard.ItemsSource = this.Pieces;
+		}
+
+        private void ChessBoard_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+			Point p = Mouse.GetPosition(ChessBoard);
+			int x = (int)p.X;
+			int y = (int)p.Y;
+
+			var currentSelect = new Point(x, y);
+			var lastSelectedPiece = Pieces.Where(z => z.Pos.X == LastSelectedPosition.X && z.Pos.Y == LastSelectedPosition.Y).FirstOrDefault();
+			var currentSelectedPiece = Pieces.Where(z => z.Pos.X == currentSelect.X && z.Pos.Y == currentSelect.Y).FirstOrDefault();
+			if (lastSelectedPiece != null && currentSelectedPiece == null)
+            {
+				lastSelectedPiece.Pos = currentSelect;
+				
+			}
+			else
+            {
+				LastSelectedPosition = currentSelect;
+			}
+			var selection = Pieces.Where(z => z.Type == PieceType.Selection).FirstOrDefault();
+			selection.Pos = currentSelect;
+
+		}
+
     }
+
+
+
 }
---
+++ Salta/PieceType.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salta
+{
+	public enum PieceType
+	{
+		Star_1,
+		Star_2,
+		Star_3,
+		Star_4,
+		Star_5,
+		Moon_1,
+		Moon_2,
+		Moon_3,
+		Moon_4,
+		Moon_5,
+		Point_1,
+		Point_2,
+		Point_3,
+		Point_4,
+		Point_5,
+		Selection
+	}
+}
