ab6282d853e824f72dfd195987627f29c22abc09
Dominik AD <dominik716@wp.pl>
2021-06-09 18:27:31 +0200

    Reversed original totalsum calculations

--- Salta/Engine.cs
+++ Salta/Engine.cs
@@ -180,7 +180,7 @@ namespace Salta
 				double y = Math.Abs(piece.FinalPos.Y - piece.Pos.Y); 
 				if (piece.Player == Player.Red)
 				{
-					totalsum -= x + y;
+					totalsum += x + y;
 				}
 				else if (piece.Player == Player.Green)
                 {
